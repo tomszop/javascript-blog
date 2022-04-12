8594a87c77e1cc3ed7c384aef2f0fc6a1f541311
Tomasz Szopa <tomszop@gmail.com>
2022-04-12 21:16:59 +0200

    Add JS function to generate title list

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+{
 'use strict'
 
 /*
@@ -48,10 +49,55 @@ document.getElementById('test-button').addEventListener('click', function(){
 
     targetArticle.classList.add('active');
     console.log(targetArticle);
+  }
 
-  }
-  const links = document.querySelectorAll('.titles a');
-  
-  for(let link of links){
-    link.addEventListener('click', titleClickHandler);
-  }
+  //generate title links
+
+  const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+    function generateTitleLinks(){
+
+  /* [DONE]remove contents of titleList */
+
+  const titleList = document.querySelector(optTitleListSelector);
+  titleList.innerHTML = '';
+ 
+  /* [DONE]for each article */
+
+  const articles = document.querySelectorAll(optArticleSelector);
+  let html = '';
+  for(let article of articles){
+
+    /* [DONE]get the article id */
+
+    const articleId = article.getAttribute("id");
+
+    /* [DONE]find the title element */
+ 
+    /* [DONE]get the title from the title element */
+
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+    /* [DONE]create HTML of the link */
+
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+
+    /* [DONE] insert link into titleList */
+
+    html = html + linkHTML;
+    }
+    titleList.innerHTML = html;
+    console.log(html);
+
+    const links = document.querySelectorAll('.titles a');
+    console.log(links)
+    
+    for(let link of links){
+      link.addEventListener('click', titleClickHandler);
+    }
+}
+    generateTitleLinks();
+
+}
